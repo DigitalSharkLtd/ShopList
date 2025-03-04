de27b8c6c38f06f25bbdbba697af78b65aac583b
DigitalSharkLtd <74692785+DigitalSharkLtd@users.noreply.github.com>
2025-03-04 02:28:15 +0200

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import ShopList from "./App";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <ShopList />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
