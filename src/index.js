1f9da193dae339cd3edcd224bf35cc9e3d5ef69f
DigitalSharkLtd <74692785+DigitalSharkLtd@users.noreply.github.com>
2025-03-04 03:19:25 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App"; // ✅ Правильный импорт
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter basename="/ShopList">  {/* ✅ Add basename */}
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </BrowserRouter>,
   document.getElementById("root")
 );
